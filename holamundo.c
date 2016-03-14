97dc81a0af836df8a20a61ae3dd866e3d09a17bf
i42agmom <i42agmom@uco.es>
2016-03-14 17:32:43 +0100

    Hola mundo modificado

--- holamundo.c
+++ holamundo.c
@@ -1,6 +1,9 @@
 #inlcude "./includes/miinclude.h"
 
 int main(){
+	char c[100] = "Hola mundo";
 	printf("Hola mundo\n");
+	printf("Adios mundo cruel\n");
+	printf("%s\n", strcat(c, ", soy i42agmom"));
 	return 0;
 }
